75669da2ddaf76145e38d975999953c644e4f5ed
Anitesh2004 <129663186+Anitesh2004@users.noreply.github.com>
2024-06-19 01:43:35 +0530

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password:"Abhyuday@ac",
+  password:"Anitesh@ac",
   database:"agriconnect"
 })
